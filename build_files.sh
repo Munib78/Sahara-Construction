fd03a944f155fa0f8276f2412b9fe45fad02a103
Munib <139039102+Munib78@users.noreply.github.com>
2024-06-30 18:47:50 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 echo " BUILD START "
 
-python3.11.2 -m pip install -r requirements.txt
-python3.11.2 manage.py collectstatic --noinput --clear
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 echo " BUILD END " 
 
 
